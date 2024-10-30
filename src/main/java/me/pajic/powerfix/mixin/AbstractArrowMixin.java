5aea740f00adfba6aed6dd2b5a5486420e2bd129
pajicadvance <pavle.pajic23@gmail.com>
2024-10-30 19:10:18 +0100

    1.21.2

--- src/main/java/me/pajic/powerfix/mixin/AbstractArrowMixin.java
+++ src/main/java/me/pajic/powerfix/mixin/AbstractArrowMixin.java
@@ -1,12 +1,16 @@
 package me.pajic.powerfix.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.item.enchantment.Enchantments;
+import net.minecraft.world.phys.EntityHitResult;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AbstractArrow.class)
 public abstract class AbstractArrowMixin {
@@ -21,9 +25,20 @@ public abstract class AbstractArrowMixin {
     )
     private float fixPowerDamage(float baseDamage) {
         AbstractArrow arrow = (AbstractArrow) (Object) this;
-        if (EnchantmentHelper.getItemEnchantmentLevel(arrow.level().registryAccess().registryOrThrow(Registries.ENCHANTMENT).getHolderOrThrow(Enchantments.POWER), arrow.getWeaponItem()) > 0) {
+        if (EnchantmentHelper.getItemEnchantmentLevel(arrow.level().registryAccess().lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(Enchantments.POWER), arrow.getWeaponItem()) > 0) {
             return baseDamage + 0.5F;
         }
         return baseDamage;
+    }
+
+    @Inject(
+            method = "onHitEntity",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/util/Mth;ceil(D)I"
+            )
+    )
+    private void debug(EntityHitResult entityHitResult, CallbackInfo ci, @Local double d) {
+        System.out.println(d);
     }
 }
